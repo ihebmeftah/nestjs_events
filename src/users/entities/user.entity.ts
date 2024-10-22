972a83c9449584459cec098cfeb7b561c70b8e3c
ihebmeftah <ihebmeftah666@gmail.com>
2024-10-22 19:17:27 +0100

    add user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,11 +1,23 @@
+import { Exclude } from 'class-transformer';
+import { UUID } from 'crypto';
+import { TimeStampBase } from 'src/generics/db/timestamp.entity';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity("users")
-export class User {
-    @PrimaryGeneratedColumn()
-    id: number;
+export class User extends TimeStampBase {
+    @PrimaryGeneratedColumn("uuid")
+    id: UUID;
     @Column()
-    name: string;
+    firstName: string;
     @Column()
+    lastName: string;
+    @Column({ unique: true })
     email: string;
+    @Column({ unique: true })
+    phone: string;
+    @Column({ nullable: true })
+    deviceToken: string;
+    @Column()
+    @Exclude()
+    password: string;
 }

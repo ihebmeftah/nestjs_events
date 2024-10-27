c6b433fd6cb3d7c6ef636d01f311361d3e20f269
ihebmeftah <ihebmeftah666@gmail.com>
2024-10-28 00:59:55 +0100

    remove module event cause word event do problem

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,16 +4,13 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { dataSourceOptions } from 'db/data-source';
-import { JwtModule } from '@nestjs/jwt';
 import { AuthModule } from './auth/auth.module';
-import { EventModule } from './event/event.module';
 
 @Module({
   imports: [
     UsersModule,
     TypeOrmModule.forRoot(dataSourceOptions),
     AuthModule,
-    EventModule,
   ],
   controllers: [AppController],
   providers: [AppService],

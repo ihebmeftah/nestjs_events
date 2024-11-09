53001cdc8c1864aae733690c86b3061a89e86540
ihebmeftah <ihebmeftah666@gmail.com>
2024-11-09 22:19:29 +0100

    Refactor main.dart to use a StatelessWidget for the app configuration
    - Created a new `App` class extending `StatelessWidget` to encapsulate the `GetMaterialApp` configuration.
    - Updated the `title` to "Events" and set the `initialRoute` to `Routes.LOGIN`.
    - Configured the `theme` for `TextButton`, `ElevatedButton`, and `InputDecoration` with specific styles and colors.
    - Removed the direct `runApp` call to `GetMaterialApp` and replaced it with the new `App` widget.

--- front_flutter/lib/main.dart
+++ front_flutter/lib/main.dart
@@ -5,11 +5,51 @@ import 'package:get/get.dart';
 import 'app/routes/app_pages.dart';
 
 void main() {
-  runApp(
-    GetMaterialApp(
-      title: "Application",
-      initialRoute: AppPages.INITIAL,
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: "Events",
+      initialRoute: Routes.LOGIN,
       getPages: AppPages.routes,
-    ),
-  );
+      theme: ThemeData(
+          textButtonTheme: TextButtonThemeData(
+              style: TextButton.styleFrom(
+            foregroundColor: Colors.pink,
+          )),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.pink,
+              foregroundColor: Colors.white,
+              fixedSize: Size(MediaQuery.of(context).size.width * 0.8, 55),
+              textStyle:
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+          ),
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+              borderRadius: BorderRadius.circular(20),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.grey.shade400),
+              borderRadius: BorderRadius.circular(15),
+            ),
+          )),
+    );
+  }
 }

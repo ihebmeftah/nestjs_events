d95380941d5a86169504b6c0f3d0a69edccbd22e
beldi5 <118887659+beldi5@users.noreply.github.com>
2024-12-02 00:10:36 +0100

    install and configure postman

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,15 @@ async function bootstrap() {
       transform: true,
     }),
   );
+  const config = new DocumentBuilder() //1
+    .setTitle("Events API")
+    .setDescription("This is an API for managing events")
+    .setVersion("1.0")
+    .build();
+  const document = SwaggerModule.createDocument(app, config); //2
+  SwaggerModule.setup("api", app, document); //3
   await app.listen(3000);
 }
 bootstrap();
+
+      

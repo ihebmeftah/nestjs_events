f6d6b070e2752354666d8c1de7ccb06f813658a7
ihebmeftah <ihebmeftah666@gmail.com>
2024-12-04 00:25:28 +0100

    Add custom API exception classes
    Introduce custom exception classes for handling different API error scenarios such as NotFound, Forbidden, BadRequest, and Conflict.

---
+++ front_flutter/lib/app/data/remote/apis_exception.dart
@@ -0,0 +1,7 @@
+class NotFoundExcpetion implements Exception {}
+
+class ForrbidenExcpetion implements Exception {}
+
+class BadRequestExcpetion implements Exception {}
+
+class ConflictExcpetion implements Exception {}

3e7b708af0b2b2675a5e98b2eeb7fa5ab951cde1
ihebmeftah <ihebmeftah666@gmail.com>
2024-12-04 13:18:03 +0100

    Add userId storage methods to Sharedpref
    Added methods to save and retrieve userId using GetStorage in the Sharedpref class. This allows for storing and accessing the user ID locally.

--- front_flutter/lib/app/data/local/sharedpref.dart
+++ front_flutter/lib/app/data/local/sharedpref.dart
@@ -2,6 +2,7 @@ import 'package:get_storage/get_storage.dart';
 
 class Sharedpref {
   final String _token = "token";
+  final String _userId = "userId";
 
   Sharedpref._internal();
 
@@ -20,6 +21,12 @@ class Sharedpref {
 
   String? get getToken => _storage.read<String>(_token);
 
+  Future<bool> saveUserId(String value) async {
+    await _storage.write(_userId, value);
+    return true;
+  }
+
+  String? get getUserId => _storage.read<String>(_userId);
   Future<bool> clearStorage() async {
     await _storage.erase();
     return true;
